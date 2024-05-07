dda809475eb9facc338541bda9a644e0274e6a9f
TY-Shanthala_Sajjan <shanthalasajjan1998@gmail.com>
2024-05-07 10:24:51 +0530

    path correct for contact us

--- components/commonComponents/textArea/TextArea.jsx
+++ components/commonComponents/textArea/TextArea.jsx
@@ -7,16 +7,17 @@ function TextArea(
         onChange,
         placeholder = '',
         iconPath = '',
-        inputStyle = ''
+        textAreaStyle = '',
+        iconStyle = ''
     }
 
 ) {
     return (
         <section className={`${sectionStyle} relative`}>
             {iconPath &&
-                <img src={iconPath} alt="icon" className="absolute left-2 top-[25%] transform -translate-y-1/2 w-6 h-6"></img>
+                <img src={iconPath} alt="icon" className={`${iconStyle} absolute left-2 top-1/2 transform -translate-y-1/2 w-6 h-6`}></img>
             }
-            <textarea className={`${inputStyle} w-full ${iconPath ? 'py-2 pl-12 pr-4' : 'p-2'} border border-gray-300 rounded`} type={type} onChange={onChange} placeholder={placeholder} />
+            <textarea className={`${textAreaStyle} w-full ${iconPath ? 'py-2 pl-12 pr-4' : 'p-2'} border border-gray-300 rounded`} type={type} onChange={onChange} placeholder={placeholder} />
         </section>
     )
 }
