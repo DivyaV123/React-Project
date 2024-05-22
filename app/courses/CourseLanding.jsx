2dc55e6955bf1586ca08fd6d8c3fa2805d35ea19
Nithish T <nithish.t@testyantra.com>
2024-05-22 15:43:59 +0530

    Responsive course page

--- app/courses/CourseLanding.jsx
+++ app/courses/CourseLanding.jsx
@@ -48,13 +48,13 @@ const CourseLanding = ({ page }) => {
       sectionStyling=" max-w-full overflow-hidden courseBackgroundwave "
       articalStyling="relative"
     >
-      <aside className="flex py-8  justify-between">
-        <article className="w-[51.56vw] flex flex-col py-4">
-          <div className="bg-white h-[2.65vw] flex w-fit classTypes">
+      <aside className="flex pt-[6.944vh]  justify-between">
+        <article className="w-[51.56vw] flex flex-col ">
+          <div className="bg-white  flex w-fit classTypes">
             {tutionClasses.map((classItem, index) => (
               <button
                 key={index}
-                className={`flex justify-center items-center px-4 py-2 font-medium text-[0.75rem] text-dark-gray ${classItem.title === btnState ? "activecourseButton" : ""
+                className={`flex justify-center items-center px-[1.25vw] py-[1.111vh] font-medium text-[0.938vw] text-dark-gray ${classItem.title === btnState ? "activecourseButton" : ""
                   } `}
                 onClick={() => {
                   setBtnState(classItem.title);
@@ -64,12 +64,12 @@ const CourseLanding = ({ page }) => {
               </button>
             ))}
           </div>
-          <div className="py-4 flex gap-6 items-center">
-            <h1 className="font-bold leading-[4.5rem]  text-left text-[3rem]">
+          <div className="py-[1.389vh] flex gap-6 items-center">
+            <h1 className="font-bold text-left text-[3.75vw]">
               Software Testing
             </h1>
             <button className="ratingButton flex justify-center items-center py-2 px-4 font-semibold rounded text-ash">
-              <span className="flex text-base p-1">
+              <span className="flex text-[1.25vw] py-[0.556vh] px-[0.313vw]">
                 {" "}
                 <Svg
                   width={svgicons.ratingStar[0]}
@@ -82,29 +82,29 @@ const CourseLanding = ({ page }) => {
               4.6 Rating
             </button>
           </div>
-          <div className="pb-6">
-            <p className="headerText text-left font-medium text-xl leading-[1.875rem]">
+          <div className="pb-[5.556vh]">
+            <p className="headerText text-left font-medium text-[1.563vw] leading-[1.875rem]">
               Explore the dynamic world of software testing with our
               comprehensive course.
             </p>
           </div>
-          <article className="flex pb-8">
+          <article className="flex pb-[5.556vh]">
             {statisticsData.map((ele, index) => (
-              <div key={index} className=" pr-8">
-                <h1 className="text-left font-bold text-[1.5rem] leading-[2.25rem] pb-2 headerText">
+              <div key={index} className=" ">
+                <h1 className={`text-left font-bold text-[1.875vw]  pb-[0.556vh] headerText ${ele.number !== "15,000+" ? "pr-[2.5vw]" :""}`}>
                   {ele.number}
                 </h1>
-                <p className="text-left font-medium text-[0.875rem] leading-[1.25rem] headerText">
+                <p className={`text-left font-medium text-[1.094vw]  headerText ${ele.text !== "IT Students placed" ? "pr-[2.5vw]" :""}`}>
                   {ele.text}
                 </p>
               </div>
             ))}
           </article>
-          <article className="pb-4">
+          <article className="pb-[2.778vh]">
             {resources.map((resource, index) => (
               <div
                 key={index}
-                className="flex text-left gap-4  text-[0.75rem] items-center leading-[1rem] pb-4 font-semibold text-ash"
+                className="flex text-left gap-4  text-[0.938vw] items-center leading-[1rem] pb-[2.778vh] font-semibold text-ash"
               >
                 <figure>
                   <img src={imageSrc} alt={`resource-${index}`} />
@@ -115,11 +115,11 @@ const CourseLanding = ({ page }) => {
               </div>
             ))}
           </article>
-          <section className="flex gap-6 pb-4">
-            <button className="EnrollButton text-base font-semibold py-2.5 px-6">
+          <section className="flex gap-6 pb-[7.222vh]">
+            <button className="EnrollButton text-[1.25vw] font-semibold py-[1.389vh] px-[1.875vw]">
               Enroll now
             </button>
-            <button className="EnquireButton  text-base font-semibold py-2.5 px-6 relative">
+            <button className="EnquireButton  text-[1.25vw] font-semibold py-[1.389vh] px-[1.875vw] relative">
               <span>Enquire now</span>
             </button>
           </section>
--- app/courses/ImageScroller.jsx
+++ app/courses/ImageScroller.jsx
@@ -61,47 +61,53 @@ const ImageScroller = ({ onRightBarFix, isRightBarFixed }) => {
       <div ref={section2Ref}>
         <img src="../courseLanding.png" alt="courseLanding" />
         {isRightBarFixed && (
-          <div className="cardDataContainer">
-            <div className="flex py-3 justify-between items-center">
-              <h1 className="font-bold leading-[1rem] text-left text-[1.5rem]">
+          <div className="py-[2.222vh] px-[1.25vw]">
+            <div className="flex  justify-between items-center">
+              <h1 className="font-bold  text-left text-[1.875vw]">
                 Software Testing
               </h1>
-              <button className="ratingButton flex justify-center items-center py-3 px-4 font-medium rounded">
-                <span className="flex text-base p-1 font-bold">
-                  <Svg
-                    width={svgicons.ratingStar[0]}
-                    height={svgicons.ratingStar[1]}
-                    viewBox={svgicons.ratingStar[2]}
-                    icon={svgicons.ratingStar[3]}
-                    color={svgicons.ratingStar[4]}
-                  />
-                </span>{" "}
-                4.6 Rating
+              <button className=" flex gap-2 text-[1.25vw] justify-center items-center py-[1.111vh] px-[0.938vw] font-medium rounded">
+                <Svg
+                  width={svgicons.ratingStar[0]}
+                  height={svgicons.ratingStar[1]}
+                  viewBox={svgicons.ratingStar[2]}
+                  icon={svgicons.ratingStar[3]}
+                  color={svgicons.ratingStar[4]}
+                />
+                <p className="text-[1.25vw] font-medium">4.6 Rating</p>
               </button>
             </div>
             <div>
-              <p className="text-left font-medium text-[0.875rem] 2xl:text-base 3xl:text-[1.125rem] headerText py-2">
+              <p className="text-left font-medium text-[1.094vw]  headerText pt-[0.781vw] pb-[2.031vw]">
                 Explore the dynamic world of software testing with our
                 comprehensive course.
               </p>
             </div>
-            <article className="flex justify-between py-1 2xl:py-2 3xl:py-5">
+            <article className="flex  pb-[4.444vh]">
               {statisticsData.map((ele, index) => (
-                <div key={index} className="py-2 pr-2">
-                  <h1 className="text-left font-bold text-[0.875rem]  3xl:text-[1.125rem] headerText">
+                <div key={index}>
+                  <h1
+                    className={`text-left font-bold text-[1.094vw]  headerText ${
+                      ele.number !== "15,000+" ? "pr-[0.781vw]" : ""
+                    }`}
+                  >
                     {ele.number}
                   </h1>
-                  <p className="text-left font-medium text-[0.625rem] tracking-tighter 2xl:text-[0.7rem] 3xl:text-[0.9rem] headerText pt-1 2xl:pt-2 3xl:pt-3">
+                  <p
+                    className={`text-left font-medium text-[0.781vw] tracking-tighter  headerText pt-[0.417vh] ${
+                      ele.text !== "IT Students placed" ? "pr-[0.781vw]" : ""
+                    }`}
+                  >
                     {ele.text}
                   </p>
                 </div>
               ))}
             </article>
-            <section className="flex gap-6 pb-2 2xl:pb-4 3xl:pb-6 pt-1 2xl:pt-2 3xl:pt-3 justify-center">
-              <button className="EnrollButton text-base 2xl:text-base font-semibold py-2.5 px-6 w-[15.15vw]">
+            <section className="flex gap-6  justify-center">
+              <button className="EnrollButton text-[1.25vw] font-semibold py-[0.781vw] px-[3.333vh] w-[15.15vw]">
                 Enroll now
               </button>
-              <button className="EnquireButton text-base 2xl:text-base font-semibold py-2.5 px-6 relative w-[15.15vw]">
+              <button className="EnquireButton text-[1.25vw] font-semibold py-[0.781vw] px-[3.333vh] relative w-[15.15vw]">
                 <span>Enquire now</span>
               </button>
             </section>
