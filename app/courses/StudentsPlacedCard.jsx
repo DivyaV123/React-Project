828d5e9b7867bd1efed6ec6ca3d301ca6e717d80
Nithish T <nithish.t@testyantra.com>
2024-05-27 10:02:25 +0530

    Responsive fix for course page below sections

--- app/courses/StudentsPlacedCard.jsx
+++ app/courses/StudentsPlacedCard.jsx
@@ -1,9 +1,10 @@
 'use client'
+import React, { useEffect, useState } from 'react'
 import MaxWebWidth from '@/components/commonComponents/maxwebWidth/maxWebWidth'
 import TrainingCardSkeleton from '@/components/ourBranches/trainingMode/TrainingCardSkeleton'
 import { Button } from '@/components/ui/button'
 import { SectionIcon } from '@radix-ui/react-icons'
-import React, { useEffect, useState } from 'react'
+import './CourseLanding.scss';
 
 function StudentsPlacedCard({ studentsInfo, page }) {
     const [isloading, setisLoading] = useState(true)
@@ -14,21 +15,21 @@ function StudentsPlacedCard({ studentsInfo, page }) {
     }, [])
 
     return (
-        <section className='flex flex-wrap gap-4 justify-between'>
+        <section className={`flex flex-wrap gap-y-[1.25vw]  ${page==='branch'?"justify-between":"px-[1.25vw] py-[2.222vh] gap-x-[2.222vh]"}`}>
             {studentsInfo.map((element) => (
-                isloading ? <TrainingCardSkeleton /> : <section className='flex justify content center '>
-                    <article className={page !== 'branch' ? 'bg-white py-[2.222vh] px-[1.25vw] rounded-lg' : 'bg-white px-[5.556vh] py-[3.125vw] rounded-lg w-[20.469vw]'} >
-                        <figure className='flex justify-center pb-[3.125vw]'>
+                isloading ? <TrainingCardSkeleton /> : <section className={`flex testinomialShadow ${page==='branch'?"w-[20.469vw]":" w-[14.688vw]"}`}>
+                    <article className={page !== 'branch' ? 'bg-white py-[1.667vh] px-[0.859vw] w-full rounded-lg' : 'bg-white pb-[7.639vh] pt-[3.75vh] px-[3.75vw] rounded-lg '} >
+                        <figure className='flex justify-center pb-[2.222vh]'>
                             <img src={element.image} />
                         </figure>
                         <header>
-                            <h1 className='flex justify-center pb-[2.778vh] font-semibold text-[1.563vw] text-dark-gray'>
+                            <h1 className={`flex justify-center  font-semibold text-dark-gray text-[1.563vw] ${page !== 'branch' ?"pb-[3.333vh]" : "pb-[2.222vh] "}`}>
                                 {element.name}
                             </h1>
                             <article className='flex justify-center'>
                                 <div>
-                                    <p className='flex justify-center text-dark-gray pb-1 text-[1.094vw]'>{element.batch}</p>
-                                    <p className='flex justify-center text-dark-gray text-[0.938vw]'>{element.course}</p>
+                                    <p className='flex justify-center text-dark-gray pb-[0.556vh] text-[1.094vw]'>{element.batch}</p>
+                                    <p className='flex justify-center text-dark-gray text-[1.094vw]'>{element.course}</p>
                                 </div>
                             </article>
                         </header>
