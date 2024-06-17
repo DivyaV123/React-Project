64f0a4a1565595ff3c0dfa6b3e1591ae1e0de1e5
Nithish T <nithish.t@testyantra.com>
2024-06-17 15:18:15 +0530

    Implemented Course menu

--- components/navHome/navItems/Courses.jsx
+++ components/navHome/navItems/Courses.jsx
@@ -1,96 +1,81 @@
 "use client";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import './navitems.scss'
-import Svg from "@/components/commonComponents/Svg/Svg";
-import { svgicons } from "@/components/assets/icons/svgassets";
-import { COURSE_PATH } from "@/lib/RouteConstants";
 import Link from "next/link";
 import { useGetAllCategoriesQuery } from "@/redux/queries/getAllCategories";
-import { GlobalContext } from "@/components/Context/GlobalContext";
-
+import { truncateText } from "@/lib/utils";
 const Courses = () => {
-  const { setSelectedCoursDetailseId } = useContext(GlobalContext);
-  const { data: response, isLoading, error } = useGetAllCategoriesQuery();
-  const [hoveredCourseIndex, setHoveredCourseIndex] = useState(0);
-  const [hoveredSubCourseIndex, setHoveredSubCourseIndex] = useState(0);
-  const [activeResponses, setActiveResponses] = useState([]);
-
-  const handleCourseHover = (courseIndex, courseResponses) => {
-    setHoveredCourseIndex(courseIndex);
-    setHoveredSubCourseIndex(null); // Reset sub-course hover
-    setActiveResponses(courseResponses || []); // Set default course responses
-  };
-
-  const handleSubCourseHover = (subCourseIndex, subCourseResponses) => {
-    setHoveredSubCourseIndex(subCourseIndex);
-    setActiveResponses(subCourseResponses || []); // Set sub-course responses on hover
-  };
-
-  const handleSubCourseLeave = () => {
-    setHoveredSubCourseIndex(null);
-    if (hoveredCourseIndex !== null) {
-      // Reset to course responses if a course is still hovered
-      setActiveResponses(response?.data[hoveredCourseIndex]?.courseResponse || []);
-    }
-  };
-
+  const {data:courseResponse,isLoading,error}=useGetAllCategoriesQuery()
+  const [hoveredIndex, setHoveredIndex] = useState(0);
+  const [hoveredItemIndex, setHoveredItemIndex] = useState(0);
+  const getAllCourses=courseResponse?.data
+  const subCourse=getAllCourses?.[hoveredIndex]?.subCourse?.length>0 && getAllCourses?.[hoveredIndex]?.subCourse
+  const subCourseContent=subCourse?.[hoveredItemIndex]?.subCourseResponse?.length>0 && subCourse?.[hoveredItemIndex]?.subCourseResponse
+  const finalContent=getAllCourses?.[hoveredIndex]?.courseResponse?.length>0 ? getAllCourses?.[hoveredIndex]?.courseResponse?.filter(ele=>ele) : subCourseContent
   return (
-    <div className="flex w-[81.09vw] lg:h-[500px] overflow-auto myscrollbar">
-      <div className="menuSidebar pt-2 xl:w-[18.75vw] 2xl:w-[14.75vw] 3xl:w-[11.75vw]">
-        {response?.data?.map((courseItem, courseIndex) => (
+    <div className="flex w-[81.09vw]  lg:h-[500px] overflow-auto myscrollbar ">
+      <div className="menuSidebar pt-2  xl:w-[18.75vw] 2xl:w-[14.75vw]  3xl:w-[11.75vw] ">
+        {getAllCourses?.map((courseItem, index) => (
           <div
-            key={courseIndex}
-            onMouseEnter={() => handleCourseHover(courseIndex, courseItem.courseResponse)}
-            className={`flex ${hoveredCourseIndex === courseIndex ? 'menuItem' : 'menuItemdisable'} pl-4 pr-2 items-center`}
+            key={index}
+            onMouseEnter={() => setHoveredIndex(index)}
+            className={`flex ${hoveredIndex  === index  ? 'menuItem' : 'menuItemdisable'}  pl-4 pr-2 items-center`}
           >
             <img src={courseItem.icon} />
             <div className="flex justify-between grow">
               <button className="p-2 text-sm">{courseItem.title}</button>
-              <img src='/arrowIconDark.svg' className={`${hoveredCourseIndex === courseIndex ? 'visible' : 'invisible'} w-4`} />
+              <img src="/arrowIconDark.svg" className={`${hoveredIndex === index ? 'visible' : 'invisible'} w-4`} />
             </div>
           </div>
         ))}
       </div>
-
-      <div className="xl:w-[62.34vw] 2xl:w-[66.34vw] 3xl:w-[70.34vw] flex">
-        {hoveredCourseIndex !== null && response?.data[hoveredCourseIndex]?.subCourse?.length !== 0 && (
-          <div className="xl:w-[20.31vw] 2xl:w-[18.5vw] 3xl:w-[13.6vw] pt-2 menuSidebar">
-            {response?.data[hoveredCourseIndex]?.subCourse?.map((subCourseItem, subCourseIndex) => (
+      <div className="xl:w-[62.34vw] 2xl:w-[66.34vw] 3xl:w-[70.34vw] flex ">
+        {hoveredIndex !== null && getAllCourses?.[hoveredIndex]?.subCourse?.length>0 && (
+          <div className="xl:w-[20.31vw] 2xl:w-[18.5vw]  3xl:w-[13.6vw]  pt-2 menuSidebar">
+            {getAllCourses?.[hoveredIndex]?.subCourse?.map((item, itemIndex) => (
               <div
-                key={subCourseIndex}
-                className={`flex justify-between grow pl-2 ${hoveredSubCourseIndex === subCourseIndex ? 'menuItem' : 'menuItemdisable'} pr-2 items-center`}
-                onMouseEnter={() => handleSubCourseHover(subCourseIndex, subCourseItem.subCourseResponse)}
-                onMouseLeave={handleSubCourseLeave}
+                key={itemIndex}
+                className={`flex justify-between grow pl-2 ${hoveredItemIndex  === itemIndex ? 'menuItem' : 'menuItemdisable'} pr-2 items-center`}
+                onMouseEnter={() => setHoveredItemIndex(itemIndex)}
+                onMouseLeave={() => {}}
               >
-                <img src={subCourseItem.icon} />
+                <img src={item?.icon} />
                 <div className="flex justify-between grow">
-                  <button className="p-2 text-sm">{subCourseItem.title}</button>
-                  <img src='/arrowIconDark.svg' className={`${hoveredSubCourseIndex === subCourseIndex ? 'visible' : 'invisible'} w-4`} />
+                  <button className="p-2 text-sm">{item?.title}</button>
+                  <img src="/arrowIconDark.svg" className={`${hoveredItemIndex === itemIndex ? 'visible' : 'invisible'} w-4`} />
                 </div>
               </div>
             ))}
           </div>
         )}
-        <div className={`${hoveredCourseIndex !== null && activeResponses.length
-          ? "courselist pl-4 pt-2"
-          : "coursefull p-3"
-          } flex flex-wrap h-fit gap-4`}>
-          {activeResponses.map((content, index) => (
-            <Link href={`/courses/${content.courseResponseId}`} key={index}>
-              <div className={`${hoveredCourseIndex !== null && activeResponses.length ? "courseMedium" : "courseinitial"} p-2 branchOverlay h-fit`}>
-                <div className="flex h-10 gap-1.5">
-                  <img className="h-8 w-8" src={content.icon} />
+        <div
+          className={`${
+            hoveredIndex !== null && subCourse
+              ? "courselist pl-4 pt-2"
+              : "coursefull p-3"
+          }   flex flex-wrap  h-fit gap-4 `}
+         >
+          {finalContent?.length > 0 && 
+          finalContent?.map(
+            (content, index) => (
+             <Link href={`/courses/${content?.courseResponseId ? content?.courseResponseId :content?.subCourseResponseId}`}> <div
+                key={index}
+                className={`${
+                  hoveredIndex !== null && subCourse
+                    ? "courseMedium"
+                    : "courseinitial"
+                }  p-2 branchOverlay h-fit`}
+              >
+                <div className="flex h-10 gap-1.5 ">
+                  <img className="h-8 w-8" src={content?.icon} />
                   <h3 className="text-left h-8 text-sm font-bold items-center flex">
-                    {content.title}
+                    {content?.title}
                   </h3>
                 </div>
                 <div>
-                  <article title={content.description} className="text-sm titleText">
-                    {content.description.length > 85 ? content.description.substring(0, 85) + '...' : content.description}
-                  </article>
+                  <article className=" text-sm titleText">{truncateText(content?.description,100)}</article>
                 </div>
-              </div>
-            </Link>
+              </div></Link>
           ))}
         </div>
       </div>
