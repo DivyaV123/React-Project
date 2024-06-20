151de8b8d9e459d290b811256831bd82c59335e6
Anil Kumar <anilkumar.p@testyantra.com>
2024-06-20 15:16:43 +0530

    SignUP UI responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,8 +61,9 @@ module.exports = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      }, screens: {
+      },screens: {
         '3xl': '1920px',
+        mobile:{'max': '639px'},
       },
       keyframes: {
         "accordion-down": {
