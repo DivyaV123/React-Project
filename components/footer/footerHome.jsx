e8cc04808fff92ef53d4182ada7f0606c17b4151
Anil Kumar <anilkumar.p@testyantra.com>
2024-06-20 18:18:54 +0530

    footer responsive

--- components/footer/footerHome.jsx
+++ components/footer/footerHome.jsx
@@ -9,32 +9,36 @@ function FooterHome() {
     const footerContent = {
         Company: ['About Us', 'News', 'Partners', 'Statistics'],
         Resources: ['Case Study', 'E - Books', 'Blogs', 'Events'],
-        ServiceAndSupport: ['Training', 'Learning', 'Hiring', 'Hackathons', 'Events', 'Customer Support'],
+        'Service & Support': ['Training', 'Learning', 'Hiring', 'Hackathons', 'Events'],
         ContactUs: ['Demos', 'Branches', 'Units']
     }
     return (
         <MaxWebWidth sectionStyling='bg-footer-blue min-h-80 flex justify-center'>
             <footer>
                 <Fade left duration={1000} delay={0}>
-                    <article className='flex justify-around pt-8 '>
-                        {Object.entries(footerContent).map(([header, subHeaders], index) => (
-                            <div key={index}>
-                                <h3 className='text-white  font-bold pb-2 text-base'>{header}</h3>
-                                <ul>
-                                    {subHeaders.map((subHeader, subIndex) => (
-                                        <li className='text-white pr-2 pb-2 pt-2 text-sm font-normal cursor-pointer' key={subIndex}>{subHeader}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ))}
-                    </article>
+                <article className='flex justify-around pt-8 mobile:grid mobile:grid-cols-2 mobile:grid-rows-1 mobile:gap-x-28'>
+    {Object.entries(footerContent).map(([header, subHeaders], index) => (
+        <div key={index} className="mobile:col-span-1 mobile:row-span-1">
+            <h3 className='text-white mobile:w-[100vw] font-bold pb-2 text-base mobile:text-[4.999vw]'> {/* Add mobile class here */}
+                {header}
+            </h3>
+            <ul className=' mobile:pb-[3.541vh]'>
+                {subHeaders.map((subHeader, subIndex) => (
+                    <li className='text-white pr-2 pb-2 pt-2 text-sm font-normal cursor-pointer mobile:text-[3.956vw]' key={subIndex}>{subHeader}</li>
+                ))}
+            </ul>
+        </div>
+    ))}
+</article>
+
+
                 </Fade>
                 <Fade bottom duration={1000} delay={0}>
                     <article className='relative b-0 p-8'>
-                        <span className='flex justify-center'>
+                        <span className='flex justify-center mobile:relative mobile:bottom-[3.863vh]'>
                             <CommonIconsComponent lite={true} />
                         </span>
-                        <span className='text-white text-xs flex justify-center'>© 2023 Qspiders All Rights Reserved</span>
+                        <span className='text-white text-xs flex justify-center mobile:relative mobile:bottom-[2.79vh] mobile:text-[3.721vw] mobile:w-[73vw]'>© 2023 Qspiders All Rights Reserved</span>
                     </article>
                 </Fade>
             </footer>
