6fdc61a4706bfb4b79fe887e5a373e3c23332205
Nithish T <nithish.t@testyantra.com>
2024-06-28 23:26:17 +0530

    Mobile design for course menu

--- app/coursenav/CoursePopup.jsx
+++ app/coursenav/CoursePopup.jsx
@@ -3,9 +3,10 @@ import React from "react";
 import Svg from "@/components/commonComponents/Svg/Svg";
 import { svgicons } from "@/components/assets/icons/svgassets";
 import Link from "next/link";
-import { NAV_BAR } from "@/lib/RouteConstants";
+import { NAV_BAR, HOME_PATH } from "@/lib/RouteConstants";
 import { useGetAllCategoriesQuery } from "@/redux/queries/getAllCategories";
 import { useRouter } from "next/navigation";
+import Loading from "@/lib/Loading";
 const CoursePopup = () => {
   const router = useRouter();
   const {
@@ -22,38 +23,46 @@ const CoursePopup = () => {
 
   return (
     <section className="w-full h-full block">
-      <section className="flex justify-between my-[2.575vh] mx-[3.721vw] items-center">
-        <Link className="flex items-center" href={NAV_BAR}>
-          <img className="" src="/icon_arrow_left.svg" />
-          <div className="font-bold text-[3.256vw]">Courses</div>
-        </Link>
-        <div className="border-none hover:bg-white p-0 ">
-          <Svg
-            className="w-[6.512vw] h-[3.004vh] outline-none"
-            width={svgicons.cancelButtonIcon[0]}
-            height={svgicons.cancelButtonIcon[1]}
-            viewBox={svgicons.cancelButtonIcon[2]}
-            icon={svgicons.cancelButtonIcon[3]}
-            color={svgicons.cancelButtonIcon[4]}
-          />
-        </div>
-      </section>
-      <section className="mx-[3.721vw] my-[0.858vh]">
-        {getAllCategories?.map((course) => (
-          <div
-            className="flex justify-between py-[1.073vh]"
-            onClick={() => handleGetAllCategories(course.courseId)}
-          >
-            <div key={course?.courseId} className="flex gap-4">
-              <img src={course.icon} />
-              <div>{course.title}</div>
-            </div>
-            <div>
-              <img src="/arrow_right.svg" />
-            </div>
-          </div>
-        ))}
-      </section>
+      {CourseIsLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <section className="flex justify-between my-[2.575vh] mx-[3.721vw] items-center">
+            <Link className="flex items-center" href={NAV_BAR}>
+              <img className="" src="/icon_arrow_left.svg" />
+              <div className="font-bold text-[3.256vw]">Courses</div>
+            </Link>
+            <Link className="border-none hover:bg-white p-0" href={HOME_PATH}>
+              <Svg
+                className="w-[6.512vw] h-[3.004vh] outline-none"
+                width={svgicons.cancelButtonIcon[0]}
+                height={svgicons.cancelButtonIcon[1]}
+                viewBox={svgicons.cancelButtonIcon[2]}
+                icon={svgicons.cancelButtonIcon[3]}
+                color={svgicons.cancelButtonIcon[4]}
+              />
+            </Link>
+          </section>
+          <section className="mx-[3.721vw] my-[0.858vh]">
+            {getAllCategories?.map((course) => (
+              <div
+                className="flex justify-between py-[1.073vh]"
+                onClick={() => handleGetAllCategories(course.courseId)}
+              >
+                <div key={course?.courseId} className="flex gap-4">
+                  <img src={course.icon} />
+                  <div className="text-[2.791vw] text-[#454545] font-semibold">
+                    {course.title}
+                  </div>
+                </div>
+                <div>
+                  <img src="/arrow_right.svg" />
+                </div>
+              </div>
+            ))}
+          </section>
+        </>
+      )}
     </section>
   );
 };
--- app/coursenav/[courseResponse]/[subcourseresponse]/SubCourseResponseLanding.jsx
+++ app/coursenav/[courseResponse]/[subcourseresponse]/SubCourseResponseLanding.jsx
@@ -1,8 +1,62 @@
 'use client'
-import React from 'react'
+import React,{useMemo} from 'react'
+import { usePathname } from "next/navigation";
+import { useGetAllCategoriesQuery } from "@/redux/queries/getAllCategories";
+import Link from 'next/link';
 const SubCourseResponseLanding = () => {
+  const {
+    data: courseResponse,
+    isLoading: CourseIsLoading,
+    error: CourseError,
+  } = useGetAllCategoriesQuery();
+  const pathname = usePathname();
+  const [courseID, subCourseID] = pathname.split('/').slice(2, 4);  
+
+  const filterSubCourseById = useMemo(() => {
+    if (!courseResponse?.data) return [];
+    const course = courseResponse.data.find(course => String(course.courseId) === String(courseID));
+    const subCourse = course?.subCourse?.find(sub => String(sub.subCourseId) === String(subCourseID));
+    return subCourse ? [subCourse] : [];
+  }, [courseResponse, courseID, subCourseID]);
+
+  const filterSubCourseTitle = filterSubCourseById?.[0]?.title;
+  const getSubCourseResponse = filterSubCourseById?.[0]?.subCourseResponse || [];
+console.log(filterSubCourseTitle,getSubCourseResponse,"updatedcode")
   return (
-    <div>SubCourseResponseLanding</div>
+    <section className="my-[1.717vh]">
+            <Link
+        className="flex gap-2.5 pl-[1.628vw] bg-[#F9F9F9] py-[1.502vh] items-center"
+        href={`/coursenav/${courseID}`}
+      >
+        <img src="/icon_arrow_left.svg" />
+        <div className="text-[3.256vw] font-semibold text-[#454545]">
+          {filterSubCourseTitle}
+        </div>
+      </Link>
+      <section className="mx-[3.721vw] my-[0.858vh]">
+        {getSubCourseResponse?.length > 0 && getSubCourseResponse.map((response) => (
+          <Link
+            key={response?.subCourseResponseId}
+            className="mx-[2.326vw] my-[1.073vh]"
+            href={`/courses/${response?.subCourseResponseId}`}
+          >
+            <div className="flex gap-2.5 pt-[0.858vh] items-center">
+              <img
+                className="w-[8.372vw] h-[3.863vh]"
+                src={response.icon}
+                alt={response.title}
+              />
+              <div className="text-[2.791vw] font-bold text-[#454545]">
+                {response.title}
+              </div>
+            </div>
+            <div className="mt-[0.858vh] py-[1.073vh] text-[2.558vw] font-semibold text-[#575757]">
+              {response.description}
+            </div>
+          </Link>
+        ))}
+      </section>
+    </section>
   )
 }
 
