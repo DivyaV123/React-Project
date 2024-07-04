4c67504e2b0520bec61207a113dc285b047dfa8d
Nithish T <nithish.t@testyantra.com>
2024-07-04 16:31:57 +0530

    Fixed cancel button issue in video player

--- app/courses/VideoPreview.jsx
+++ app/courses/VideoPreview.jsx
@@ -6,13 +6,14 @@ import {
 import "./CourseLanding.scss";
 import Svg from "@/components/commonComponents/Svg/Svg";
 import { svgicons } from "@/components/assets/icons/svgassets";
-const VideoPreview = ({ videoUrl}) => {
+const VideoPreview = ({ videoUrl,setVideoPreview}) => {
   return (
     <div className="">
       <AlertDialogContent className="sm:max-w-[62.5vw]  rounded-xl mobile:h-[50vh] mobile:w-[80vw] mobile:p-0">
-        <AlertDialogCancel className="border-none  p-0 absolute right-2 mobile:right-0 mobile:-top-[0.5rem] cursor-pointer " >
+        <AlertDialogCancel className="border-none z-10  p-0 absolute right-2 mobile:right-0 mobile:-top-[0.5rem] cursor-pointer " >
           <Svg
-          className='w-[5.953vw] h-[3.472vh] sm:w-[1.953vw]'
+          className='w-[5.953vw] h-[3.472vh] sm:w-[1.953vw] cursor-pointer'
+          onClick={()=>setVideoPreview(false)}
             width={svgicons.cancelButtonIcon[0]}
             height={svgicons.cancelButtonIcon[1]}
             viewBox={svgicons.cancelButtonIcon[2]}
