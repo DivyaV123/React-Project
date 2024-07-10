927e016f65995fb2cc71d6729700e584294f9fca
TY-Shanthala_Sajjan <shanthalasajjan1998@gmail.com>
2024-07-10 15:11:36 +0530

    image optimisation

--- app/universityPrograms/ProgramDescription.jsx
+++ app/universityPrograms/ProgramDescription.jsx
@@ -6,6 +6,7 @@ import "@/components/ui/button.scss";
 import Svg from "@/components/commonComponents/Svg/Svg";
 import { svgicons } from "@/components/assets/icons/svgassets";
 import EnrollPopUp from "@/components/commonComponents/courseCard/EnrollPopUp";
+import { object } from "yup";
 const ProgramDescription = ({
   courseDetails,
   countDetails,
@@ -48,27 +49,27 @@ const ProgramDescription = ({
   };
   const reasonToChoose = [
     {
-      icon: '../illustrate_tickmark.svg', 
+      icon: '../illustrate_tickmark.svg',
       text: "Flexible learning schedule"
     },
     {
-      icon: '../illustrate_tickmark.svg', 
+      icon: '../illustrate_tickmark.svg',
       text: "Real-life projects and practical exposure"
     },
     {
-      icon: '../illustrate_tickmark.svg', 
+      icon: '../illustrate_tickmark.svg',
       text: "Alumni network of 40,000 members"
     },
     {
-      icon: '../illustrate_tickmark.svg', 
+      icon: '../illustrate_tickmark.svg',
       text: "Save up to INR 27Lacks compare to full-time master's"
     },
     {
-      icon: '../illustrate_tickmark.svg', 
+      icon: '../illustrate_tickmark.svg',
       text: "Learn with students from 80+ Countries"
     },
     {
-      icon: '../illustrate_tickmark.svg', 
+      icon: '../illustrate_tickmark.svg',
       text: "Dedicated Program Manager"
     }
   ];
@@ -125,7 +126,12 @@ const ProgramDescription = ({
                       className="flex text-left gap-4  items-center  pb-[2.778vh] mobile:pb-[1.073vh]  text-ash"
                     >
                       <figure>
-                        <img src={imageSrc} alt={`resource-${index}`} />
+                        <Image
+                          style={{ objectFit: 'cover' }}
+                          width={300}
+                          height={400}
+                          src={imageSrc}
+                          alt={`resource-${index}`} />
                       </figure>
                       <article>
                         <p className="text-[1vw] font-semibold mobile:text-[2.791vw]">
@@ -153,7 +159,8 @@ const ProgramDescription = ({
                 </section>
               </div>
               <div>
-                <img
+                <Image
+                  style={{ objectFit: 'cover' }}
                   src={courseDetails?.courseImage}
                   className="w-[28.125vw] h-[60.417vh] sm:mb-[2.222vh] mobile:w-full mobile:h-[26.18vh] mobile:pb-[3.219vh]"
                   alt="CourseImage"
@@ -161,54 +168,54 @@ const ProgramDescription = ({
               </div>
             </section>
           </article>
-         
+
         </aside>
       </MaxWebWidth>
       <article className="courseTiming w-full">
-      <div className=" mobile:w-[92.558vw] m-auto flex w-[87.5vw] h-[19.444vh] mt-[5.556vh] courseTime justify-evenly items-center">
-            <section className="w-[22.166vw] h-[11.111vh] flex flex-col justify-center border-r-4 border-orange-500">
-              <label className="text-[1.1vw] w-[12vw] flex justify-center font-semibold">Learning Format</label>
-              <p className="text-[1.363vw] text-orange-600 font-bold">Online Bootcamp</p>
-            </section>
-            <section className="w-[22.166vw] h-[11.111vh] flex flex-col justify-center border-r-4 border-orange-500">
-              {" "}
-              <label className="text-[1.1vw] w-[8vw]  text-nowrap flex justify-center font-semibold">Program Duration</label>
-              <p className="text-[1.363vw] text-orange-600 font-bold">11 months</p>
-            </section>
-            <section className="">
-              {" "}
-              <label className="text-[1.1vw] font-semibold">Starting Date</label>
-              <p className="text-[1.363vw] text-orange-600 font-bold">6 july,2024</p>
-            </section>
-            </div>
-          </article>
+        <div className=" mobile:w-[92.558vw] m-auto flex w-[87.5vw] h-[19.444vh] mt-[5.556vh] courseTime justify-evenly items-center">
+          <section className="w-[22.166vw] h-[11.111vh] flex flex-col justify-center border-r-4 border-orange-500">
+            <label className="text-[1.1vw] w-[12vw] flex justify-center font-semibold">Learning Format</label>
+            <p className="text-[1.363vw] text-orange-600 font-bold">Online Bootcamp</p>
+          </section>
+          <section className="w-[22.166vw] h-[11.111vh] flex flex-col justify-center border-r-4 border-orange-500">
+            {" "}
+            <label className="text-[1.1vw] w-[8vw]  text-nowrap flex justify-center font-semibold">Program Duration</label>
+            <p className="text-[1.363vw] text-orange-600 font-bold">11 months</p>
+          </section>
+          <section className="">
+            {" "}
+            <label className="text-[1.1vw] font-semibold">Starting Date</label>
+            <p className="text-[1.363vw] text-orange-600 font-bold">6 july,2024</p>
+          </section>
+        </div>
+      </article>
 
-          <article className="reasonToChoose w-full">
-          <section className="mobile:w-[92.558vw] m-auto  w-[87.5vw]">
+      <article className="reasonToChoose w-full">
+        <section className="mobile:w-[92.558vw] m-auto  w-[87.5vw]">
           <header className="text-[2.5vw] mt-[5.556vh] flex font-bold  justify-center">Why Choose this Master’s in Human Computer Interaction ?</header>
           <div className="flex flex-wrap justify-center gap-8 p-4 mt-[4vh]">
-      {reasonToChoose.map((item, index) => (
-        <div key={index} className="flex items-center gap-4 w-[34vw]">
-          <img src={item.icon} alt={item.text} width={24} height={24} />
-          <span className="text-[#575757] text-[1.2vw] font-semibold text-nowrap">{item.text}</span>
-        </div>
-      ))}
-    </div>
-          </section>
+            {reasonToChoose.map((item, index) => (
+              <div key={index} className="flex items-center gap-4 w-[34vw]">
+                <img src={item.icon} alt={item.text} width={24} height={24} />
+                <span className="text-[#575757] text-[1.2vw] font-semibold text-nowrap">{item.text}</span>
+              </div>
+            ))}
+          </div>
+        </section>
 
-          </article>
-          <article className="ProgramCurriculum w-full">
-          <section className="mobile:w-[92.558vw] m-auto  w-[87.5vw]">
-            <header className="text-[2.5vw]  flex font-bold  justify-start mt-[6.111vh]">Program Curriculum</header>
-            <p className="text-[1.094vw] mt-[2vh] text-[#575757] font-medium">
+      </article>
+      <article className="ProgramCurriculum w-full">
+        <section className="mobile:w-[92.558vw] m-auto  w-[87.5vw]">
+          <header className="text-[2.5vw]  flex font-bold  justify-start mt-[6.111vh]">Program Curriculum</header>
+          <p className="text-[1.094vw] mt-[2vh] text-[#575757] font-medium">
             In collaboration with Clark University, this comprehensive MSCS hybrid program, Master of Science in Computer Science, is perfect for both fresh graduates and experienced professionals to build a successful career in tech. Complete your online courses and then get on-campus immersion in the USA, enabling you to gain the full benefit of the Clark University student experience and U.S. job opportunities. Get your MS in Computer Science from Clark University and join its global alumni network.
-            </p>
-            <button className="EnquireButton mt-[5vh] mb-[2vh] text-[1.25vw] font-semibold py-[1.389vh] px-[1.875vw] relative">
-                    <span>Download Curriculum</span>
-                  </button>
-          </section>
+          </p>
+          <button className="EnquireButton mt-[5vh] mb-[2vh] text-[1.25vw] font-semibold py-[1.389vh] px-[1.875vw] relative">
+            <span>Download Curriculum</span>
+          </button>
+        </section>
 
-          </article>
+      </article>
       {/* <ImageScroller cardData={courseDetails} onRightBarFix={handleRightBarFix} isRightBarFixed={isRightBarFixed} /> */}
       {/* <EnrollPopUp isModalOpen={isModalOpen} handleCloseModal={handleCloseModal} /> */}
     </>
