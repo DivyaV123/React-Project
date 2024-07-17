941bbe0732b2bf04ba535888ea889de233bd8d8e
Anil Kumar <anilkumar.p@testyantra.com>
2024-07-17 18:45:23 +0530

    subject adder ui completed

--- app/subjectAdder/SubTopicForm.jsx
+++ app/subjectAdder/SubTopicForm.jsx
@@ -5,12 +5,70 @@ import * as Yup from 'yup';
 import Input from '@/components/commonComponents/input/Input';
 
 const SubTopicForm = ({ initialValues, onUpdateSubTopic }) => {
- 
+  console.log({initialValues})
+  const formik = useFormik({
+    initialValues: {
+      subTopicTitle: initialValues?.subTopicTitle || '',
+      subTopicPreviewUrl: initialValues?.subTopicPreviewUrl || '',
+      subTopicPreviewDuration: initialValues?.subTopicPreviewDuration || '',
+    },
+   
+    validationSchema: Yup.object({
+      subTopicTitle: Yup.string().required('Required'),
+      // subTopicPreviewUrl: Yup.string().url('Invalid URL').required('Required'),
+      // subTopicPreviewDuration: Yup.number().required('Required'),
+    }),
+   
+  });
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    formik.setFieldValue(name, value);
+    formik.handleChange(e);
+    onUpdateSubTopic({
+      ...formik.values,
+      [name]: value,
+    });
+  };
   return (
 
       
         <div className='m-auto border mt-4 mb-4 border-gray-300 p-8 rounded-xl'>
-          subTopic
+          <form onSubmit={formik.handleSubmit}>
+            <div>
+              <label><span className="text-red-500 pr-1 ">*</span> SubTopic Name</label>
+              <Input
+                type="text"
+                name="subTopicTitle"
+                onChange={handleFormChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.subTopicTitle}
+              />
+              {formik.errors.subTopicTitle ? <div>{formik.errors.subTopicTitle}</div> : null}
+            </div>
+            <div>
+              <label>URL</label>
+              <Input
+                type="text"
+                name="subTopicPreviewUrl"
+                onChange={handleFormChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.subTopicPreviewUrl}
+              />
+              {formik.errors.subTopicPreviewUrl ? <div>{formik.errors.subTopicPreviewUrl}</div> : null}
+            </div>
+            <div>
+              <label>Video Duration</label>
+              <Input
+                type="text"
+                name="subTopicPreviewDuration"
+                onChange={handleFormChange}
+            onBlur={formik.handleBlur}
+                value={formik.values.subTopicPreviewDuration}
+              />
+              {formik.errors.subTopicPreviewDuration ? <div>{formik.errors.subTopicPreviewDuration}</div> : null}
+            </div>
+            {/* <button className="py-2 px-4 bg-gradient rounded-md text-white" type="submit">Submit SubTopic</button> */}
+          </form>
         </div>
       
     
--- app/subjectAdder/TopicForm.jsx
+++ app/subjectAdder/TopicForm.jsx
@@ -6,12 +6,103 @@ import SubTopicForm from './SubTopicForm';
 import Input from '@/components/commonComponents/input/Input';
 
 const TopicForm = ({ initialValues, onUpdateTopic }) => {
-  
+    const [subTopics, setSubTopics] = useState(initialValues.subTopics || []);
+  const formik = useFormik({
+   
+        initialValues: {
+          topicTitle: initialValues?.topicTitle || '',
+          topicPreviewUrl: initialValues?.topicPreviewUrl || '',
+          topicPreviewDuration: initialValues?.topicPreviewDuration || '',
+        },
+    validationSchema: Yup.object({
+      topicTitle: Yup.string().required('Required'),
+      // topicPreviewUrl: Yup.string().url('Invalid URL').required('Required'),
+      // topicPreviewDuration: Yup.number().required('Required'),
+    }),
+   
+  });
+
+
+
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    formik.setFieldValue(name, value);
+    onUpdateTopic({
+      ...formik.values, // Pass current form values
+      [name]: value,    // Ensure the latest changed field is updated
+    });
+  };
+
+  const handleAddSubTopic = () => {
+    // formik.handleSubmit()
+    formik.values.topicTitle && setSubTopics([
+      ...subTopics,
+      { subTopicTitle: '', subTopicPreviewUrl: '', subTopicPreviewDuration: '' },
+    ]);
+  };
+
+  const handleUpdateSubTopic = (updatedSubTopic, index) => {
+    const updatedSubTopics = [...subTopics];
+    updatedSubTopics[index] = updatedSubTopic;
+    setSubTopics(updatedSubTopics);
+    onUpdateTopic({
+      ...formik.values,
+      subTopics: updatedSubTopics,
+    });
+  };
 
   return (
     <div className='m-auto border border-gray-300 p-8 mt-4 mb-4 rounded-xl'>
      
-      Topic form 
+        <div >
+          <form onSubmit={formik.handleSubmit}>
+            <div>
+              <label><span className="text-red-500 pr-1 ">*</span>Topic Name</label>
+              <Input
+                type="text"
+                name="topicTitle"
+                onChange={handleFormChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.topicTitle}
+              />
+              {formik.errors.topicTitle ? <div className='text-red-500 text-[0.75vw]'>{formik.errors.topicTitle}</div> : null}
+            </div>
+            <div>
+              <label>URL</label>
+              <Input
+                type="text"
+                name="topicPreviewUrl"
+                onChange={handleFormChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.topicPreviewUrl}
+              />
+              {formik.errors.topicPreviewUrl ? <div>{formik.errors.topicPreviewUrl}</div> : null}
+            </div>
+            <div>
+              <label>Video Duration</label>
+              <Input
+                type="text"
+                name="topicPreviewDuration"
+                onChange={handleFormChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.topicPreviewDuration}
+              />
+              {formik.errors.topicPreviewDuration ? <div>{formik.errors.topicPreviewDuration}</div> : null}
+            </div>
+            {subTopics.map((subTopic, index) => (
+          <SubTopicForm
+            key={index}
+            initialValues={subTopic}
+            onUpdateSubTopic={(updatedSubTopic) => handleUpdateSubTopic(updatedSubTopic, index)}
+          />
+        ))}
+            <button type="button" className="py-2 mt-3 px-4 bg-gradient rounded-md text-white" onClick={handleAddSubTopic}>
+          Add SubTopic
+        </button>
+          
+          </form>
+        </div>
     
     </div>
   );
