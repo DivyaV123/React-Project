cf6546fd298f0f9b6acc7a8f55e20d8bd1b5acbb
TY-Shanthala_Sajjan <shanthalasajjan1998@gmail.com>
2024-08-02 16:51:19 +0530

    searc and add btn design and sub category page

--- app/adminportal/[organisation]/NavTabs.jsx
+++ app/adminportal/[organisation]/NavTabs.jsx
@@ -4,6 +4,9 @@ import { GlobalContext } from "@/components/Context/GlobalContext";
 import "./AdminSidebar.scss";
 import { usePathname, useRouter } from "next/navigation";
 import { ADMIN_PORTAL } from "@/lib/RouteConstants";
+import Input from "@/components/commonComponents/input/Input";
+import Svg from "@/components/commonComponents/Svg/Svg";
+import { svgicons } from "@/components/assets/icons/svgassets";
 const NavTabs = ({ categoryData }) => {
   const router = useRouter();
   const pathname = usePathname();
@@ -35,20 +38,46 @@ const NavTabs = ({ categoryData }) => {
     router.push(`${ADMIN_PORTAL}/${sidebarParam},${item.title}`);
   };
   return (
-    <section className="flex gap-4 mt-[1.389vh] mb-[4.111vh] pl-[1.875vw]">
-      {domains.map((item, index) => (
-        <div
-          key={index}
-          className={`pt-[0.972vh] pb-[1.528vh] cursor-pointer px-[0.625vw]  ${instituteParam === item.title
-            ? "text-[#FF7B1B] font-bold activeTab"
-            : " font-medium text-[#212121] inActiveTab"
-            }`}
-          onClick={() => handleNavTab(item)}
-        >
-          <h1 className="text-[1.094vw] font-bold">{item.title}</h1>
+    <>
+      <section className="flex gap-4 mt-[1.389vh]  pl-[1.875vw]">
+        {domains.map((item, index) => (
+          <div
+            key={index}
+            className={`pt-[0.972vh] pb-[1.528vh] cursor-pointer px-[0.625vw]  ${instituteParam === item.title
+              ? "text-[#FF7B1B] font-bold activeTab"
+              : " font-medium text-[#212121] inActiveTab"
+              }`}
+            onClick={() => handleNavTab(item)}
+          >
+            <h1 className="text-[1.094vw] font-bold">{item.title}</h1>
+          </div>
+        ))}
+      </section>
+      <article className="flex justify-between">
+        <div className="pt-[2.222vh] pl-[1.875vw] w-[29.688vw]">
+          <Input
+            inputStyle="rounded-md"
+            placeholder="search"
+            iconPath="../images/icon_outline_search.png"
+          />
         </div>
-      ))}
-    </section>
+        <aside className="pt-[2.778vh] pr-[1.875vw]">
+          <div className=" bg-gradient rounded-md py-[1.111vh] px-[0.938vw] text-white font-bold flex gap-2 text-[1.094vw]">
+            <Svg
+              width={svgicons.addIcon[0]}
+              height={svgicons.addIcon[1]}
+              viewBox={svgicons.addIcon[2]}
+              icon={svgicons.addIcon[3]}
+              color={svgicons.addIcon[4]}
+            />
+            <button
+              className=""
+            >Sub Category</button>
+          </div>
+        </aside>
+
+      </article>
+    </>
   );
 };
 
