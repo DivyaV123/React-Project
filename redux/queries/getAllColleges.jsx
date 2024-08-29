29c7a4107a4495065ea845e2b8767d7563e34518
Nithish T <nithish.t@testyantra.com>
2024-08-29 17:26:06 +0530

    Implemented State and College in internal placements section

--- redux/queries/getAllColleges.jsx
+++ redux/queries/getAllColleges.jsx
@@ -1,16 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { API_ENDPOINTS, getPlacementUrl } from '@/api/apiService';
+import { API_ENDPOINTS, getStatisticsUrl } from '@/api/apiService';
 
 export const collegesApi = createApi({
     reducerPath: 'collegesApi',
-    baseQuery: fetchBaseQuery({ baseUrl: getPlacementUrl() }),
+    baseQuery: fetchBaseQuery({ baseUrl: getStatisticsUrl() }),
     endpoints: (builder) => ({
         getAllColleges: builder.query({
-            query: (university) => ({
-                url: API_ENDPOINTS.GET_COLLEGES,
-                method: 'POST',
-                body: university
-            })
+            query: (id,district_id,state_id,university_id) => API_ENDPOINTS.GET_COLLEGES(id,district_id,state_id,university_id),
         }),
     }),
 });
