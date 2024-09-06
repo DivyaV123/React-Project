fb4eda580e4516f4ee81bf2151d9da66b0039e59
Nithish T <nithish.t@testyantra.com>
2024-09-06 20:13:43 +0530

    Reimplemented explore our courses

--- redux/queries/getHomePageCourse.jsx
+++ redux/queries/getHomePageCourse.jsx
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { getBaseUrl,API_ENDPOINTS } from '@/api/apiService';
+import { getWebsiteUrl,API_ENDPOINTS } from '@/api/apiService';
 
 export const getHomePageCourseApi = createApi({
     reducerPath: 'getHomePageCourseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
+    baseQuery: fetchBaseQuery({ baseUrl: getWebsiteUrl() }),
     endpoints: (builder) => ({
         getHomePageCourse: builder.query({
             query: () => API_ENDPOINTS.FIND_ALL_CATEGORIES,
