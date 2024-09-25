6c1103dc473c527427d212f8242fc8208f3dffd6
Nithish T <nithish.t@testyantra.com>
2024-09-25 17:16:33 +0530

    Reimplemented Get Course by Id

--- redux/queries/getCoursedetails.jsx
+++ redux/queries/getCoursedetails.jsx
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { getBaseUrl,API_ENDPOINTS } from '@/api/apiService';
+import { getWebsiteUrl,API_ENDPOINTS } from '@/api/apiService';
 
 export const courseDetailsApi = createApi({
     reducerPath: 'courseDetailsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
+    baseQuery: fetchBaseQuery({ baseUrl: getWebsiteUrl() }),
     endpoints: (builder) => ({
         getAllCourseDetails: builder.query({
             query: (courseId) =>API_ENDPOINTS.GET_ALL_COURSES_BY_COURSE_ID(courseId) ,
